373bd3b56c479816072c996bd98718c5393222ea
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-07-31 11:43:44 +0000

    problem2 solved

--- module16-10/practice-problem-javascript/problem2.js
+++ module16-10/practice-problem-javascript/problem2.js
@@ -1,15 +1,29 @@
-var momGaveHarry = 1000;
-var orangePricePerKg = 400;
-var numOfOrangeKg = 1;
-var grapesPricePerKg = 300;
-var numOfGrapeKg = 1;
+// all subjects numbers
 
-var lastMoneyInHarryPocket =
-  momGaveHarry -
-  (orangePricePerKg * numOfOrangeKg + grapesPricePerKg * numOfGrapeKg);
+var math = 75.25;
+var biology = 65;
+var chemisty = 80;
+var physics = 35.45;
+var bangla = 99.5;
 
-console.log(lastMoneyInHarryPocket);
+var totalMarks = math + biology + chemisty + physics + bangla;
+/*
+console.log(totalMarks);
+/* 
+➜ /workspaces/milestone4-module16-hello-javascript/module16-10/practice-problem-javascript (main ✗) $ node problem2.js 
+355.2 */
+
+var avgMarks = totalMarks / 5;
+/*
+console.log(avgMarks);
+/* 
+➜ /workspaces/milestone4-module16-hello-javascript/module16-10/practice-problem-javascript (main ✗) $ node problem2.js 
+71.03999999999999 */
+
+// printing the result in 2 decimal places
+avgMarks = avgMarks.toFixed(2);
+console.log(avgMarks);
 
 /* 
-@RaisulislamRimon ➜ /workspaces/milestone4-module16-hello-javascript/module16-10/practice-problem (main ✗) $ node problem1.js 
-300 */
+➜ /workspaces/milestone4-module16-hello-javascript/module16-10/practice-problem-javascript (main ✗) $ node problem2.js 
+71.04 */
