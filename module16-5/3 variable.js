c02791b09d63c1bf1f1f8462c5c9e872385f7faa
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-07-31 06:17:48 +0000

    module16-5

---
+++ module16-5/3 variable.js
@@ -0,0 +1,31 @@
+// vary + able = variable
+
+// numeric
+
+var price = 11; // price is a variable
+var age = 17; // age is a variable
+var temperature = 38; // temperature is a variable
+
+var weight = 70; //  kg = kg
+var money = 40; // taka (thousand taka) = taka
+
+// string
+
+var name = "Rimon"; // string   = name
+
+console.log(price, age, temperature, weight, money);
+console.log(name);
+
+var special = "I am special";
+console.log(special);
+
+
+// Boolean data type = true / false = 1 / 0 
+
+var serious = true; 
+var isFullMarks = true;
+var isPass = true;
+var isFail = false;
+var isSingle = true;
+
+console.log(serious, isFullMarks, isPass, isFail, isSingle);
---
+++ module16-5/README.md
@@ -0,0 +1,4 @@
+# milestone4-module16-hello-javascript
+### introduction to javascript
+
+### 16-5 JavaScript Keyword, Variable name naming convention
