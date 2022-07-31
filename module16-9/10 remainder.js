226b6a7cd9a050afc2d0b456391b61e3ba8c8ba5
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-07-31 11:18:22 +0000

    module16-9

---
+++ module16-9/10 remainder.js
@@ -0,0 +1,22 @@
+var mangoes = 7;
+var person = 2;
+
+// var remainder = mangoes % person;    // remainder = 1
+
+/* 
+    / => this means vagfol
+    % => this means modulo/vagshesh 
+*/
+/*console.log(mangoes / person);
+/* @RaisulislamRimon ➜ /workspaces/milestone4-module16-hello-javascript/module16-9 (main ✗) $ node 10\ remainder.js 
+3.5 */
+/*
+console.log(mangoes % person);
+/* @RaisulislamRimon ➜ /workspaces/milestone4-module16-hello-javascript/module16-9 (main ✗) $ node 10\ remainder.js 
+1 */
+console.log(mangoes / person);
+console.log(mangoes % person);
+/* 
+@RaisulislamRimon ➜ /workspaces/milestone4-module16-hello-javascript/module16-9 (main ✗) $ node 10\ remainder.js 
+3.5
+1 */
