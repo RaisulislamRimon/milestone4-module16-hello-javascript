aacc0c9736d0c3f3e4ea8e6d5ad9534d36553d7b
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-07-31 06:04:56 +0000

    update

--- module16-3/3 variable.js
+++ module16-3/3 variable.js
@@ -1,10 +1,10 @@
 // vary + able = variable
 
 var price = 11; // price is a variable
-var age = 17;   // age is a variable
-var temperature = 38;   // temperature is a variable
+var age = 17; // age is a variable
+var temperature = 38; // temperature is a variable
 
-var weight = 70; //  kg = kg    
+var weight = 70; //  kg = kg
 var money = 40; // taka (thousand taka) = taka
 var name = "Rimon"; // string   = name
 
@@ -13,3 +13,11 @@ console.log(name);
 
 var special = "I am special";
 console.log(special);
+
+var serious = true;
+var isFullMarks = true;
+var isPass = true;
+var isFail = false;
+var isSingle = true;
+
+console.log(serious, isFullMarks, isPass, isFail, isSingle);
