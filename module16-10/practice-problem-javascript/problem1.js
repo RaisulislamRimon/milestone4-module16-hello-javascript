93aa82509a48d9a64eac7a9ffee8ca9b5c29a80b
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-07-31 11:31:33 +0000

    javascript practice problem solving

---
+++ module16-10/practice-problem-javascript/problem1.js
@@ -0,0 +1,15 @@
+var momGaveHarry = 1000;
+var orangePricePerKg = 400;
+var numOfOrangeKg = 1;
+var grapesPricePerKg = 300;
+var numOfGrapeKg = 1;
+
+var lastMoneyInHarryPocket =
+  momGaveHarry -
+  (orangePricePerKg * numOfOrangeKg + grapesPricePerKg * numOfGrapeKg);
+
+console.log(lastMoneyInHarryPocket);
+
+/* 
+@RaisulislamRimon ➜ /workspaces/milestone4-module16-hello-javascript/module16-10/practice-problem (main ✗) $ node problem1.js 
+300 */
