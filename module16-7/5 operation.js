9faa1f5fc5e464c944e10476bf2a5875f7be62ec
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-07-31 10:16:15 +0000

    module16-7

---
+++ module16-7/5 operation.js
@@ -0,0 +1,47 @@
+/* var onionPrice = 20;
+var eggPrice = 10;
+
+console.log("Onion price: " + onionPrice);
+console.log("Egg price: " + eggPrice);
+
+console.log(onionPrice + eggPrice);
+ */
+
+/* 
+// addition
+
+var onionPrice = 20;
+var eggPrice = 10;
+
+var total = onionPrice + eggPrice;
+
+console.log("Total = ", total);
+console.log("Total = " + total);
+ */
+/* 
+// subtraction
+var onionPrice = 20;
+var eggPrice = 10;
+
+var priceDifference = onionPrice - eggPrice;
+
+console.log(priceDifference);
+ */
+/* 
+// multiplication
+
+var orangePrice = 10;
+var quantity = 7;
+
+var totalCost = orangePrice * quantity;
+
+console.log(totalCost);
+ */
+
+// division
+var money = 500;
+var player = 10;
+
+var eachPlayer = money / player;
+
+console.log(eachPlayer);
---
+++ module16-7/README.md
@@ -0,0 +1,4 @@
+# milestone4-module16-hello-javascript
+### introduction to javascript
+
+### 16-7 (advanced) Mathematical operation shorthand
