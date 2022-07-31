c62f296fba1308b88ce60fa9469050207ca1b9fb
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-07-31 10:18:53 +0000

    module16-8

---
+++ module16-8/6 shorthand.js
@@ -0,0 +1,48 @@
+// variable declaration
+var tomatoPrice = 25;
+// variable update
+tomatoPrice = 30;
+
+// add something to the previous value
+var price1 = 31;
+var price2 = 10;
+
+// set a new value to the variable
+// price1 = 35;
+
+// add some value to the existing value
+price1 = price1 + 10;
+console.log(price1);
+
+// addition to the same variable shorthand
+// += means adding some value to the existing variable
+price1 += 10;
+console.log(price1);
+
+price2 = price2 - 5;
+console.log(price2);
+// shorthand
+// -= means deducting some value to the current value of the variable
+price2 -= 5;
+console.log(price2);
+
+// *=
+price1 *= 2;
+console.log(price1);
+
+//     /=
+price1 /= 4;
+console.log(price1);
+
+var age = 145;
+// ++ means adding 1 to the existing value
+//  increament
+age++;
+console.log(age);
+
+var love = 100;
+love = love - 1;
+// -- means you are deducting 1 from the exising value
+//  decreament
+love--;
+console.log(love);
---
+++ module16-8/README.md
@@ -0,0 +1,5 @@
+# milestone4-module16-hello-javascript
+### introduction to javascript
+
+### 16-8 (advanced) String Concatenation, type conversion parseInt parseFloat
+
