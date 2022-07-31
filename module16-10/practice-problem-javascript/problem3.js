f70a55ea7c40c8e74ea41390d2fbf0784871e70e
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-07-31 11:49:23 +0000

    problem3 solved

---
+++ module16-10/practice-problem-javascript/problem3.js
@@ -0,0 +1,16 @@
+var firstLine = "I am going to be";
+var lastLine = "an awesome JavaScript programmer & Web Developer";
+
+/*
+var resultInOneLine = firstLine + lastLine;
+console.log(resultInOneLine);
+/* 
+➜ /workspaces/milestone4-module16-hello-javascript/module16-10/practice-problem-javascript (main ✗) $ node problem3.js 
+I am going to bean awesome JavaScript programmer & Web Developer */
+
+var resultInOneLine = firstLine + " " + lastLine;
+console.log(resultInOneLine);
+
+/* 
+➜ /workspaces/milestone4-module16-hello-javascript/module16-10/practice-problem-javascript (main ✗) $ node problem3.js 
+I am going to be an awesome JavaScript programmer & Web Developer */
