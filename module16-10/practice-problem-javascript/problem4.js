94f25d71b79a07aff039d25371ae1efa1d52258b
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-07-31 11:53:29 +0000

    problem4 solved

---
+++ module16-10/practice-problem-javascript/problem4.js
@@ -0,0 +1,11 @@
+// remainder problem
+var givenNumber = 119;
+var divingNumber = 5;
+
+var remainder = givenNumber % divingNumber;
+
+console.log(remainder);
+
+/* 
+➜ /workspaces/milestone4-module16-hello-javascript/module16-10/practice-problem-javascript (main ✗) $ node problem4.js 
+4 */
