2f5a75d166b0dd1a1a55e07bc102635dbfe7fe6e
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-07-31 06:45:59 +0000

    update

---
+++ module16-6/5 operation.js
@@ -0,0 +1,16 @@
+/* var onionPrice = 20;
+var eggPrice = 10;
+
+console.log("Onion price: " + onionPrice);
+console.log("Egg price: " + eggPrice);
+
+console.log(onionPrice + eggPrice);
+ */
+
+var onionPrice = 20;
+var eggPrice = 10;
+
+var total = onionPrice + eggPrice;
+
+console.log("Total = ", total);
+console.log("Total = " + total);
---
+++ module16-6/README.md
@@ -0,0 +1,5 @@
+# milestone4-module16-hello-javascript
+### introduction to javascript
+
+### 16-6 Simple Mathematical operations in JavaScript
+
